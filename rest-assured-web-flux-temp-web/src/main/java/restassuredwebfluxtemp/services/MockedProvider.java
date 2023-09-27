2a104cc8d70fec8bd2f5d124fccc2a494ebb2105
Daniel Casey <dan_spooner15@hotmail.co.uk>
2023-09-27 13:42:57 +0100

    update to algin with base template

--- rest-assured-web-flux-temp-web/src/main/java/restassuredwebfluxtemp/services/MockedProvider.java
+++ rest-assured-web-flux-temp-web/src/main/java/restassuredwebfluxtemp/services/MockedProvider.java
@@ -9,6 +9,7 @@ import reactor.core.publisher.Mono;
 import restassuredwebfluxtemp.models.MultipleDataResponses;
 import restassuredwebfluxtemp.models.SingleDataResponse;
 
+import static restassuredwebfluxtemp.common.Constants.CONTENT_TYPE;
 import static restassuredwebfluxtemp.common.Constants.CORRELATION_ID_HEADER;
 
 public class MockedProvider {
@@ -21,19 +22,20 @@ public class MockedProvider {
         this.apiClient = apiClient;
     }
 
-    public Mono<SingleDataResponse> getSingleData(String correlationId, Integer idValue) {
+    public Mono<SingleDataResponse> getSingleData(String correlationId, Integer idValue, String contentType) {
         return apiClient
                 .get()
                 .uri("/rest-assured-template/service/data/single/{idValue}", idValue)
                 .header(CORRELATION_ID_HEADER, correlationId)
+                .header(CONTENT_TYPE, contentType)
                 .accept(MediaType.parseMediaType(MediaType.APPLICATION_JSON_VALUE))
                 .retrieve()
                 .bodyToMono(SingleDataResponse.class)
-                .doOnNext(response -> logger.info(String.format("Service Response Body: " + response.getSingleData()))
-                );
+                .switchIfEmpty(Mono.defer(() -> Mono.error(new Exception("EMPTY BODY RECEIVED"))))
+                .doOnNext(response -> logger.info(String.format("Service Response Body: " + response.getSingleData())));
     }
 
-    public Flux<MultipleDataResponses> getMultipleData(String correlationId, Integer idValue) {
+    public Flux<MultipleDataResponses> getMultipleData(String correlationId, Integer idValue, String contentType) {
         return null;
     }
 
